cf89756d86024dab112924b5b160985b4ac7de24
b3s <b.schmidleitner@gmail.com>
2019-07-13 20:27:01 +0200

    gkl file upload form - working nr2

--- js/form.js
+++ js/form.js
@@ -8,11 +8,12 @@ $(document).ready(function(){
 	// show category in form input topic
 	let showTopic = $("#topicTitle").html();
 	$("#topic").attr("value", showTopic);
+	//alert(showTopic);
 
 
 	// upload form -> show fileName in input-field
     $('input[type="file"]').change(function(e){
-        var fileName = e.target.files[0].name;
+        let fileName = e.target.files[0].name;
         $(".custom-file-label").html("<b>" + fileName + "</b>");
     });
 
--- php/simpleUpload.php
+++ php/simpleUpload.php
@@ -16,6 +16,58 @@ if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $uploadfile)) {
 echo 'Weitere Debugging Informationen:';
 print_r($_FILES);
 
+
+echo "<br>";
+print_r($uploadfile);
+echo "<br>";
+print_r($_POST['topic']);
+echo "<br>";
+print_r($_POST['author']);
+echo "<br>";
+print_r($_POST['description']);
+
+$topic = $_POST['topic'];
+$author = $_POST['author'];
+$description = $_POST['description'];
+
 print "</pre>";
+
+
+
+
+
+//add to database
+// Verbindung aufnehmen
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ba-learning-db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// echo "Connected successfully";
+
+$sql = "INSERT INTO GKL (filename, ptah, author, description)
+VALUES ('superfile', 'uploads/gkl/', '$author', '$description')";
+
+
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+$conn->close();
+
+
+
 
 ?>
