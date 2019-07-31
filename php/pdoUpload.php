8692f39c1796e99e214f2b476052b1cb5a29a659
bernd <b.schmidleitner@gmail.com>
2019-07-31 17:47:29 +0200

    database connection: conversion from mysqli to PDO

--- php/pdoUpload.php
+++ php/pdoUpload.php
@@ -40,18 +40,26 @@ $password = "";
 $dbname = "ba-learning-db";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new PDO("mysql:host=localhost; dbname=ba-learning-db", $username, $password);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$error = $conn->errorInfo();
+if ($error[0] > 0) {
+    print "Fehlercode: " .$error[1]."<br>".$error[2];
 }
+
+
+
+
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
 // echo "Connected successfully";
 
 // check whether the file is already in db
 $sqllook = "SELECT * FROM $topic WHERE filename = '$fileToUpload'";
 $result = $conn->query($sqllook);
-if ($result->num_rows > 0) {  // todo prepared statement
+if ($result->rowCount() > 0) {  // todo prepared statement
     $alreadyStored = true;
     print_r ("\$alreadyStored: $alreadyStored");
     echo "<br>";
@@ -72,33 +80,34 @@ if($alreadyStored === false){
 
 
 
-$conn->close();
+$conn = null;
 
 
 
 function tableInsert(){
-    global $conn, $sql, $topic, $fileToUpload, $uploaddir, $author, $description;
+    global $conn, $sql, $topic, $fileToUpload, $uploaddir, $author, $description, $error;
     $sql = "INSERT INTO $topic (filename, ptah, author, description)
     VALUES ('$fileToUpload', '$uploaddir', '$author', '$description')";
 
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        print "Fehlercode: " .$error[1]."<br>".$error[2];
     }
+
 }
 
 // upload file -----------------------------------------------------S
 function uploadFile(){
     global $uploadfile, $topic;
-    print_r("\$uploadfile: $uploadfile");
+    print_r("\$uploadfile: $uploadfile <br>");
     if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $uploadfile)) {
         echo "Datei ist valide und wurde erfolgreich hochgeladen.\n";
     } else {
         echo "Möglicherweise eine Dateiupload-Attacke!\n";
     }
 
-    echo 'Weitere Debugging Informationen:';
+    echo "<br> Weitere Debugging Informationen:";
     print_r($_FILES);
 
 
